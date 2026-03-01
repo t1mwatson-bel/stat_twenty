038e8dea1255ac8bb204ba4d2c47d021df3b4273
t1mwatson-bel <mix3005@yandex.ru>
2026-03-01 16:54:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ import telebot
 TOKEN = "8357635747:AAGAH_Rwk-vR8jGa6Q9F-AJLsMaEIj-JDBU"
 CHANNEL_ID = "-1003179573402"
 MAIN_URL = "https://1xlite-7636770.bar/ru/live/twentyone/1643503-twentyone-game"
-MAX_BROWSERS = 5
-CHECK_INTERVAL = 60
+MAX_BROWSERS = 2
+CHECK_INTERVAL = 30
 # =====================
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
